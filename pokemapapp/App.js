d9331aa596a0d02c178547e5c8603c2250d63fbd
Chacko <chackop@gmail.com>
2020-02-15 13:18:02 +0000

    added main app component

--- pokemapapp/App.js
+++ pokemapapp/App.js
@@ -1,10 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignIn from './src/SignIn';
+import Meteor, { createContainer, Accounts } from 'react-native-meteor';
+import PokeMap from './src/PokeMap';
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(true);
+
+  useEffect(() => {
+    console.log('connecting');
+    Meteor.connect(SERVER_URL);
+    console.log(Meteor.userId())
+    if (Meteor.userId()) {
+      flipLogin(true);
+    }
+  });
+
+  const flipLogin = (x) => {
+    setLoggedIn(x)
+  }
+
+  const signIn = (email, password) => {
+    Meteor.loginWithPassword(email, password, (error, data) => {
+      if (error) {
+        if (error.reason === "User not found") {
+          console.log('there was no email');
+          Accounts.createUser({ email, password }, (error) => {
+            console.log(error);
+          })
+        }
+      }
+      else {
+        console.log('email');
+        //TODO
+        flipLogin(true);
+      }
+    });
+    console.log(Meteor.userId())
+  }
+
+  const renderView = () => {
+    if (!loggedIn) {
+      return (
+        <SignIn signIn={signIn} />
+      )
+    }
+    else {
+      return (
+        <PokeMap flipLogin={flipLogin} />
+      )
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {renderView()}
     </View>
   );
 }
@@ -12,8 +62,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
